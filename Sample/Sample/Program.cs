de335311a407f70106277874798633dfc2a0723c
dominohades <danilomabulac01@gmail.com>
2017-04-24 14:16:53 +0800

    Add name

--- Sample/Sample/Program.cs
+++ Sample/Sample/Program.cs
@@ -13,6 +13,7 @@ namespace Sample
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("School Management System");
+            Console.WriteLine("Dans");
             Console.Read();
             
         }

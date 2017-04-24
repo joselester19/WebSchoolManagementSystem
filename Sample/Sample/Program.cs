bbbc90e12fd1346a05a4b736fda93068ce998204
joselester19 <lheitutz019@yahoo.com>
2017-04-24 11:26:34 +0800

    sample push

--- Sample/Sample/Program.cs
+++ Sample/Sample/Program.cs
@@ -10,7 +10,7 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Garci");
+            Console.WriteLine("Hello Garcia");
             Console.ReadKey();
         }
     }

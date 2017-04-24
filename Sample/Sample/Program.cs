9c91737dced15cf5334bda4c0a0944a050b74627
joselester19 <lheitutz019@yahoo.com>
2017-04-24 14:06:38 +0800

    Add Project

--- Sample/Sample/Program.cs
+++ Sample/Sample/Program.cs
@@ -12,6 +12,7 @@ namespace Sample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("School Management System");
             Console.Read();
             
         }
